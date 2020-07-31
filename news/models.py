2464c9d64167ca9a1f705543f821d165064a9da6
Alan mc Gee <mcgee.al@gmail.com>
2020-07-31 15:19:16 +0100

    setting up article admin

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+
+    name = models.CharField(max_length=254)
+    content = models.TextField()
+    link = models.CharField(max_length=254)
+    date = models.DateField()
+    provider = models.CharField(max_length=254)
+
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('date',)
--- news/views.py
+++ news/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Article
 
 def news(request):
-    ''' function to render news page'''
-    return render (request, 'news/index.html')
+    ''' function to render news page with news feed from db'''
+    articles = Article.objects.all().order_by('-date')
+    context={
+        'articles':articles
+    }
+    return render (request, 'news/index.html', context)
