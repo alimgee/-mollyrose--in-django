4e5452d34ff0bb9af7185ae7d71788a1b7cd0214
Alan mc Gee <mcgee.al@gmail.com>
2020-08-31 18:03:40 +0100

    Added model for HP Notice section

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Notice(models.Model):
+
+    name = models.CharField(max_length=254)
+    content = models.TextField()
+    link = models.CharField(max_length=254)
+    link_title = models.CharField(max_length=254)
+    
+    def __str__(self):
+        return self.name
+
