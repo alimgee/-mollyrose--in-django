5cb1ef5a1bd084d360a11e852d9a84ad999de375
Alan mc Gee <mcgee.al@gmail.com>
2020-08-31 18:08:13 +0100

    Added boolean check to Notice model

--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ class Notice(models.Model):
     content = models.TextField()
     link = models.CharField(max_length=254)
     link_title = models.CharField(max_length=254)
+    show = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
